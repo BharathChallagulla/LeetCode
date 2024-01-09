59c7189bc0fbbe491f53c7b387a4187be3de7fde
Bharath Challagulla <chlgbk2000@gmail.com>
2024-01-09 16:43:14 -0600

    217. Contains Duplicate

---
+++ 217_Contains Duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        num_set = set(nums)
+        if len(num_set)<len(nums):
+            return True
+        return False
